cb5547f671c5115cb4fadc6f3a0c4504884ba327
AlejandroSZIOSA <ggato33@gmail.com>
2023-11-21 09:10:41 +0100

    first commit

---
+++ radioplayer.js
@@ -0,0 +1,8 @@
+// Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
+
+// Steg 2. loopa med tex forEach över data.channels - ta ut data och visa på html-sidan.
+
+// Steg 3. ta ut liveaudio.url från varje kanal och lägg i en audio tagg.
+// <audio controls>
+//   <source src="" type="audio/mpeg" />
+// </audio>
